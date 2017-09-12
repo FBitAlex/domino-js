561e1096f1185bbb7985453d9cea8ce7d5c75799
Alex <argusdav@gmail.com>
2017-09-12 22:08:10 +0300

    add to table and delete from user bones

--- functions.js
+++ functions.js
@@ -6,22 +6,22 @@ var
 	rand_num = 0, // временное хранилище для раздачи
 	cnt_bone = 28,
 
-	table = {}, // массив стола
 	left_bone = -1,
 	right_bone = -1,
 
-	baza = [],
-	bazar = cnt_bone - (gamer_cnt * 7),
-	bazar_bones = {},
-	access_bones = {},
+	baza = [], // 28 сгенеренных элементов в начале игры перед раздачей
+	// bazar = cnt_bone - (gamer_cnt * 7),
+	// bazar_bones = {},
+	access_bones = [],
 	go_game = true,
 
 /*======================================= */
 	gamers_bones = [],
 	base_bone_cnt = 28,
 	first_bone = -1,
-	first_gamer = 0, // кто первый ходит
-	dashboard = $('#dashboard');
+	current_gamer = 0, // кто первый ходит
+	dashboard = $('#dashboard'),
+	table = []; // массив стола
 
 // инициализация базы
 function init () {
@@ -37,14 +37,28 @@ function init () {
 }
 
 // раздача костяшек игрокам
+function create_bazar_and_table () {
+	$('#dashboard .gamer-bons-block-1')
+		.after("<div id='bazar'><div class='bazar_bone'></div></div>")
+		.after("<div id='table'><ul></ul></div>");
+		
+	// берем с базара кость
+	$(".bazar_bone").click(function () {
+		//	console.log('CLICK');
+	});
+
+}
+
+// раздача костяшек игрокам
 function razdacha () {
 
-	var search_cht = base_bone_cnt-1,
+	var search_cnt = base_bone_cnt-1,
 		buffer_bone;
 	
 	for (gamer = 1; gamer <= gamer_cnt; gamer++) {
 		
-		
+		console.log ("num > " + gamer);
+
 		dashboard.append("<div class='gamer-bons-block-" + gamer + " gamer-bones'><ul></ul></div>");
 		var i = 0,
 		ul_list = $(".gamer-bons-block-" + gamer + ' ul');
@@ -53,7 +67,7 @@ function razdacha () {
 
 		while ( i < 7 ) {
 
-			rand_num = Math.floor(Math.random() * (search_cht + 1));
+			rand_num = Math.floor(Math.random() * (search_cnt + 1));
 			gamers_bones[gamer].push( baza[rand_num] );
 			
 			// отрисовываем элемент
@@ -62,7 +76,7 @@ function razdacha () {
 			// удалили из базы
 			baza.splice(rand_num, 1);
 			i++;
-			search_cht--;
+			search_cnt--;
 		}
 	}
 }
@@ -76,56 +90,55 @@ function find_first () {
 
 			if ( (first_bone == -1) || (l_num == r_num) && ( first_bone.l != first_bone.r ) ) {
 				first_bone = gamers_bones[gamer_num][bone];
-				first_gamer = gamer_num;
+				bone_num = bone;
+				current_gamer = gamer_num;
 
 			} else if ( (l_num == r_num) && ( first_bone.l == first_bone.r ) &&  ( l_num < first_bone.l ) ) {
 				first_bone = gamers_bones[gamer_num][bone];
-				first_gamer = gamer_num;
+				bone_num = bone;
+				current_gamer = gamer_num;
 			
 			} else if ( ( l_num < first_bone.l ) && ( first_bone.l != first_bone.r ) ) {
 				first_bone = gamers_bones[gamer_num][bone];
-				first_gamer = gamer_num;
+				bone_num = bone;
+				current_gamer = gamer_num;
 			
 			} else if ( ( r_num < first_bone.r ) && ( first_bone.l != first_bone.r ) ) {
 				first_bone = gamers_bones[gamer_num][bone];
-				first_gamer = gamer_num;	
+				bone_num = bone;
+				current_gamer = gamer_num;	
 			}
 			
 		}
 	}
-	console.log("first_gamer > " + first_gamer);
+
+	// gamers_bones[current_gamer][first_bone]["is_play"] = true;
+	console.log("bone_num >" + bone_num);
+	console.log("current_gamer > " + current_gamer);
 	console.log(first_bone);
+
+	console.dir(gamers_bones);
+	put_bone_to_table( current_gamer, bone_num );
 }
 
 
-		// проверка на меньший дубль дубль
-// 		if ( ((baza[rand_num].l == baza[rand_num].r) && (baza[last_bone].l != baza[last_bone].r))|| (last_bone == -1) ) {
-// 				last_bone = rand_num;
-// 				first_gamer = baza[rand_num][2];
-				
-// //						}
-// 		} else if ( (baza[rand_num].l == baza[rand_num].r) && (baza[last_bone].l == baza[last_bone].r) && (baza[rand_num].l < baza[last_bone].l) ) {
-// 				last_bone = rand_num;
-// 				first_gamer = baza[rand_num][2];
+function put_bone_to_table ( current_gamer, bone_num ) {
+	
+	// добавление кости в массив стола
+	table.push(gamers_bones[current_gamer][bone_num]);
 
-// 		// сумму (наименьшая цифра)
-// 		} else if ( (baza[rand_num].l < baza[last_bone].l) && (baza[last_bone].l != baza[last_bone].r) ) {
-// 				last_bone = rand_num;
-// 				first_gamer = baza[rand_num][2];
+	// перерисовка стола (добавить условие на проверку того в какой край ложить кость)
+	$('#table ul').append("<li><div class='l'>" + gamers_bones[current_gamer][bone_num].l + " </div><div class='r'>" + gamers_bones[current_gamer][bone_num].r + " </div></li>");
 
-// 		} else {
+	// удаление кости с массива игрока
+	gamers_bones[current_gamer].splice(bone_num, 1);
+	console.dir(table);
 
-// 			sum_rand_num = baza[rand_num].l + baza[rand_num].r; // 2
-// 			sum_last = baza[last_bone].l + baza[last_bone].r; // 0
+	// перерисовка руки игрока
+	var bn = bone_num + 1;
+	$('.gamer-bons-block-' + current_gamer + ' li:nth-child('+ bn +')').remove();
 
-
-// 			if ( (sum_rand_num < sum_last) && (baza[last_bone].l != baza[last_bone].r) ) {
-
-// 				last_bone = rand_num;
-// 				first_gamer = baza[rand_num][2];
-// 			 }
-
-// 		}	
+}
 
 
 // отрисовка стола 
@@ -137,25 +150,3 @@ function find_first () {
 // 		curr_gamer.append('<>')
 // 	}
 // }
-
-/*
-baza = {
-	gamer1 : {
-		l: 
-		r: 
-	},
-	
-	gamer2 : {
-
-	},
-
-	table : {
-
-	},
-
-	bazar : {
-
-	},
-	first_gamer : 
-}
-*/
--- script.js
+++ script.js
@@ -6,11 +6,14 @@ $(document).ready( function() {
 	// раздача костей
 	razdacha();
 	
-	console.dir(baza);
-	console.dir(gamers_bones);
+	// console.dir(baza);
+	// console.dir(gamers_bones);
 	
+	create_bazar_and_table();
+
 	// поиск первого хода
 	find_first(gamers_bones);
+
 
 });
 
